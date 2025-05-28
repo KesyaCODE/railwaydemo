382b957fbada868698938ae1dba75088b3a38b47
KesyaCODE <kesyapujiatmoko@gmail.com>
2025-05-28 10:31:01 +0700

    new

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/login', function () {
+    return view('login');
+})->middleware(['auth'])->name('dashboard');
